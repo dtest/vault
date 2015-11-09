dd4f7417bdc83d2dc542cd859169529940d5e8ab
Derek Downey <downey@pythian.com>
2015-11-09 10:55:08 -0500

    Initial commit

---
+++ scripts/initiate_vault.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Setup environment
+shopt -s expand_aliases
+alias vault='docker exec -it vault vault "$@"'
+export VAULT_HOST=$(echo $DOCKER_HOST | grep -Eo '\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b'):8200
+
+# Initialize
+OUTPUT="$( { vault init -key-shares=1 -key-threshold=1 | head -n2; } 2> /dev/null)"
+
+VAULT_KEY=$(echo "$OUTPUT" | grep 'Key' | awk -F': ' '{print $2}')
+export VAULT_TOKEN=$(echo "$OUTPUT" | grep 'Root Token' | awk -F': ' '{print $2}')
+
+# Unseal
+vault unseal $VAULT_KEY
