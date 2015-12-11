13887e17803d224f45d0889b68b9f131c86b7307
Derek Downey <downey@pythian.com>
2015-12-11 11:18:21 -0500

    better parsing of vault token

--- scripts/initiate_vault.sh
+++ scripts/initiate_vault.sh
@@ -9,7 +9,7 @@ export VAULT_HOST=$(echo $DOCKER_HOST | grep -Eo '\b(?:[0-9]{1,3}\.){3}[0-9]{1,3
 OUTPUT="$( { vault init -key-shares=1 -key-threshold=1 | head -n2; } 2> /dev/null)"
 
 VAULT_KEY=$(echo "$OUTPUT" | grep 'Key' | awk -F': ' '{print $2}')
-export VAULT_TOKEN=$(echo "$OUTPUT" | grep 'Root Token' | awk -F': ' '{print $2}')
+export VAULT_TOKEN=$(echo "$OUTPUT" | grep 'Root Token' | awk -F': ' '{print $2}' | tr -d )
 
 # Unseal
 vault unseal $VAULT_KEY
